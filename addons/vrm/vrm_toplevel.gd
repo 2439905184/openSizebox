75908a5a47b1565472a48e0c145655620a714d49
2439905184 <suchenglong018@126.com>
2021-09-03 14:44:13 +0800

    摄像机旋转有问题
    摄像机旋转有问题

--- addons/vrm/vrm_toplevel.gd
+++ addons/vrm/vrm_toplevel.gd
@@ -17,37 +17,49 @@ var speed=2
 #掉落速度
 var drop_speed=8
 #onready var third=$thrid
-onready var foot=$foot
+var foot
+onready var anim=$anim
+var rotate_speed=0.09
+func _ready():
+	foot=$foot
+	print_debug(foot)
+	pass
 func _process(delta):
-	if Input.is_action_pressed("grow"):
-		self.scale+=Vector3(del,del,del)
-	if Input.is_action_pressed("small"):
-		self.scale-=Vector3(del,del,del)
-	if Input.is_action_just_pressed("jump"):
-		self.translation.y+=10
-	if self.translation.y>0:
-		self.translation.y-=drop_speed*delta
-		print_debug("下落")
-	if Input.is_action_pressed("walk"):
-		self.translate(Vector3(0,0,-speed*delta))
-		if !foot.is_playing():
-			foot.play()
+	if self.name=="あやめ":
+		if Input.is_action_pressed("grow"):
+			self.scale+=Vector3(del,del,del)
+		if Input.is_action_pressed("small"):
+			self.scale-=Vector3(del,del,del)
+		if Input.is_action_just_pressed("jump"):
+			self.translation.y+=10
+		if self.translation.y>0:
+			self.translation.y-=drop_speed*delta
+			print_debug("下落")
+		if Input.is_action_pressed("walk"):
+			var rot=lerp(self.rotation_degrees.y,$Skeleton/h.rotation_degrees.y,rotate_speed)
+			self.rotation_degrees.y=rot
+			$Skeleton.translate_object_local(Vector3(0,0,-speed*delta))
+			#.rotated(Vector3.UP,$h.rotation.y))
+			if !anim.is_playing():
+				anim.play("walk")
+			if foot!=null:
+				if !foot.is_playing():
+					foot.play()
 			#.rotated(Vector3(0,1,0),deg2rad(third.rotation_degrees.y)))
-		pass
 			#self.rotation_degrees.y=third.rotation_degrees.y
 			
 #		else:
 #			self.translate(Vector3(0,0,-speed*delta))
 			#self.translate(Vector3(0,0,-speed*delta).rotated(Vector3(0,1,0),deg2rad($thrid.rotation_degrees.y)))
 func _input(event):
-	if event is InputEventMouseButton:
-		if event.button_index==BUTTON_MIDDLE:
-			$face.make_current()
-		if event.button_index==BUTTON_RIGHT:
-			$h/v/Camera.make_current()
-	if event.is_action_pressed("r"):
-		get_tree().reload_current_scene()
-	pass
+	if self.name=="あやめ":
+		if event is InputEventMouseButton:
+			if event.button_index==BUTTON_MIDDLE:
+				$face.make_current()
+			if event.button_index==BUTTON_RIGHT:
+				$h/v/Camera.make_current()
+		if event.is_action_pressed("r"):
+			get_tree().reload_current_scene()
 class VRMUtil:
 	static func from_to_rotation(from: Vector3, to: Vector3):
 		var axis: Vector3 = from.cross(to)
--- script/Third.gd
+++ script/Third.gd
@@ -4,12 +4,23 @@ var mouse_sense=0.005
 #反向
 var invert_y = false
 var invert_x = false
+var zoom_speed = 0.09
+var max_zoom=3.0
+var min_zoom=0.5
+var zoom=1.5
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	pass
 func _input(event):
+	#缩小
+	if event.is_action_pressed("zoom_in"):
+		zoom -= zoom_speed
+	if event.is_action_pressed("zoom_out"):
+		zoom += zoom_speed
+	zoom = clamp(zoom,min_zoom,max_zoom)
 	if event.is_action_pressed("esc"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	#相机旋转代码
 	if event is InputEventMouseMotion:
 		if event.relative.x != 0:
 			var dir = 1 if invert_x else -1
@@ -20,9 +31,7 @@ func _input(event):
 		if event.relative.y!=0:
 			var dir=1 if invert_y else -1
 			var y_rotation = clamp(event.relative.y, -30, 30)
-			#rotate_object_local(Vector3.UP,event.relative.x*mouse_sense)
 			$v.rotate_object_local(Vector3.RIGHT, dir*y_rotation * mouse_sense)
 func _process(delta):
-	#$h.rotation_degrees.y=camrot_h
-	#$h/v/Camera.rotation_degrees.x=camrot_v
+	scale=lerp(scale, Vector3.ONE * zoom, zoom_speed)
 	pass
