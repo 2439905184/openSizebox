d3af64a1e02a861d3f4798bd16927c8d5d6086bc
2439905184 <suchenglong018@126.com>
2021-09-11 17:40:34 +0800

    完善玩家碰撞检测和跳跃
    完善玩家碰撞检测和跳跃

--- script/player/player.gd
+++ script/player/player.gd
@@ -10,15 +10,38 @@ onready var se_fly = $fly
 var grow_speed = 0.05
 var player_state:Label
 var p_state_t = "玩家状态机:"
+var jumping = false
+var apply_gravity = true
+#线性速度
+var linear_velocity = Vector3()
+#实时f3显示线性速度
+export (NodePath)var linear_path
+var label_linear:Label
 func _ready():
 	player_state = get_parent().get_node("player_state")
+	label_linear = get_node(linear_path)
+	#print_debug(linear_path)
+	#print_debug(label_linear)
 	pass
+var gravity = Vector3(0,-9.8,0)
 func _process(delta):
+	#线性重力
+	linear_velocity += gravity * delta
+	#纵向速度
+	var vv = linear_velocity.y
 	#括号后面的是并列条件
-	if state == all_state.fly_float and (Input.is_action_pressed("jump") or Input.is_action_pressed("e")):
+	if state == all_state.fly_float and Input.is_action_just_pressed("jump"):
+		state = all_state.fly
+		apply_gravity = false
+		player_state.text = p_state_t + "fly"
+	#垂直飞行
+	if state == all_state.fly and (Input.is_action_pressed("jump") or Input.is_action_pressed("e")):
 		move_and_slide(Vector3(0,speed*delta,0))
 		if !se_fly.is_playing():
 			$fly.play(0.08)
+	if state == all_state.fly and Input.is_action_just_released("jump"):
+		state = all_state.fly_float
+		player_state.text = p_state_t +"fly float"
 	if state == all_state.fly_float and Input.is_action_pressed("q"):
 		move_and_slide(Vector3(0,-speed*delta,0))
 		if !se_fly.is_playing():
@@ -28,21 +51,25 @@ func _process(delta):
 		var pos_y = fly_float()
 		#print_debug(pos_y)
 		translate(Vector3(0,pos_y,0))
-	if state == all_state.jump and Input.is_action_just_pressed("jump"):
-		#跳
+	#跳起
+	if state == all_state.idle and Input.is_action_just_pressed("jump"):
+		print_debug("跳起")
+		vv = 10.0
+		move_and_slide(linear_velocity,Vector3(0,9.8,0))
+		state = all_state.drop
 		pass
+	#不理解为什么两个写一起
+	if apply_gravity:
+		linear_velocity = Vector3.UP * vv
+		linear_velocity = move_and_slide(linear_velocity, Vector3(0,9.8,0))
+	label_linear.text = "线性速度:"+str(linear_velocity)
 	#重力下落
 	if state == all_state.drop:
 		if !is_on_floor():
 			move_and_slide(Vector3(0,-drop_speed,0),Vector3.UP)
 		if is_on_floor():
 			state = all_state.idle
-	
-	#根据第三人称相机的指向飞行
-#	if state == all_state.fly_float and Input.is_action_pressed("walk"):
-#		move_and_slide(Vector3(0,0,-speed*delta).rotated(Vector3.UP,)
-#		$anata/anim.play("fly")
-#		pass
+			apply_gravity = true
 	if Input.is_action_just_pressed("walk"):
 		if state == all_state.fly_float:
 			state = all_state.idle
@@ -99,3 +126,12 @@ func show_size():
 	
 func _on_Tab_track_enable():
 	$anata/h.track_mouse=true
+#用于重构的同步状态代码
+func sync_state_label(value:String):
+	player_state.text = p_state_t + value
+	pass
+#根据第三人称相机的指向飞行
+#	if state == all_state.fly_float and Input.is_action_pressed("walk"):
+#		move_and_slide(Vector3(0,0,-speed*delta).rotated(Vector3.UP,)
+#		$anata/anim.play("fly")
+#		pass
